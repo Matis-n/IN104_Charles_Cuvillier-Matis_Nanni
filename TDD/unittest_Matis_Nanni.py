94628409a1fd6ff9d506f94e1dadc6d9d70c307a
mnanni <matis.nanni@ensta-paris.fr>
2021-04-22 20:21:37 +0200

    Updated

--- TDD/unittest_Matis_Nanni.py
+++ TDD/unittest_Matis_Nanni.py
@@ -1,16 +1,12 @@
 import unittest
+import class_charles_cuvillier.py
 
-import Equipe
-import Football
-
-
-bayern=Football("Bayern de Munich",100000,["Neuer"],6,"Neuer")
+bayern=Football("Bayern de Munich",5500000,22,6,"Neuer")
 
 bayern.but()
-#golden_state_warriors=Basketball(6,"Curry")
 
+golden_state_warriors=Basketball("Golden State",100000500,6,12,"Curry")
 
-
-#class TestSum(unittest.TestCase):
-#	def test_nbr_players(self):
+class TestSum(unittest.TestCase):
+	def test_nbr_players(self):
 
