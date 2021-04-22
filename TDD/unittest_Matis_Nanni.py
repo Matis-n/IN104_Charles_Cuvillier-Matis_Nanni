cac0579d2b76d9cae7886ec168d9f272093a693d
mnanni <matis.nanni@ensta-paris.fr>
2021-04-22 19:04:13 +0200

    Create unittest_Matis_Nanni.py

---
+++ TDD/unittest_Matis_Nanni.py
@@ -0,0 +1,16 @@
+import unittest
+
+import Equipe
+import Football
+
+
+bayern=Football("Bayern de Munich",100000,["Neuer"],6,"Neuer")
+
+bayern.but()
+#golden_state_warriors=Basketball(6,"Curry")
+
+
+
+#class TestSum(unittest.TestCase):
+#	def test_nbr_players(self):
+
