5593f1338b78fda117125f5cc05074fc48ef29fe
mnanni <matis.nanni@ensta-paris.fr>
2021-04-27 15:43:52 +0200

    Update

--- TDD/unittest_Matis_Nanni.py
+++ TDD/unittest_Matis_Nanni.py
@@ -1,12 +1,24 @@
 import unittest
-import class_charles_cuvillier.py
+from class_charles_cuvillier import *
 
-bayern=Football("Bayern de Munich",5500000,22,6,"Neuer")
 
 bayern.but()
 
 golden_state_warriors=Basketball("Golden State",100000500,6,12,"Curry")
 
 class TestSum(unittest.TestCase):
-	def test_nbr_players(self):
+	def test_ajout_budget(self):
 
+		bayern=Football("Bayern de Munich",5500000,22,[1985,2002,2014],"Neuer")
+
+		bayern.ajout_budget(1000000)
+		self.assertEqual(bayern.budget,6500000)
+
+	def test_transfert(self):
+		bayern=Football("Bayern de Munich",5500000,22,[1985,2002,2014],"Neuer")
+		bayern.transfert("Messi",80)
+		self.assertEqual(bayern.budget,5500000-80)
+
+
+if __name__ == '__main__':
+    unittest.main()
