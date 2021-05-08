700d17a9b0c0c7944898f15f215c87f3d49194f7
Matis Nanni <matis@8octobre.net>
2021-05-08 22:09:34 +0200

    Add Files

---
+++ GoodGPS/app/build/generated/source/buildConfig/debug/com/example/goodgps/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.goodgps;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.goodgps";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from the variant API
+  public static final String MAPS_API_KEY = "AIzaSyB_EOJQqygfn5bm1erfheusko3b1J8cSRc";
+}
